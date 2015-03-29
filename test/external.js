0840739649eec0a756871f533a67d8599b909d9f
Michael Wiencek <mwtuea@gmail.com>
2015-03-29 01:55:10 -0500

    add test for implicit external chains

--- test/external.js
+++ test/external.js
@@ -32,3 +32,28 @@ test('external, reversed bundle order', function (t) {
         t.ok(bufferEqual(buf, fs.readFileSync('external/output1.js')));
     });
 });
+
+test('implicit external chains', function (t) {
+    t.plan(3);
+
+    // b1 provides lib
+    var b1 = yarb('external/chain1.js');
+
+    // b2 explicitly requires lib from b1
+    var b2 = yarb('external/chain2.js').external(b1);
+
+    // b3 implicitly requires lib from b1 via b2
+    var b3 = yarb('external/chain3.js').external(b2);
+
+    b1.bundle(function (err, buf) {
+        t.ok(bufferEqual(buf, fs.readFileSync('external/chain-output1.js')));
+    });
+
+    b2.bundle(function (err, buf) {
+        t.ok(bufferEqual(buf, fs.readFileSync('external/chain-output2.js')));
+    });
+
+    b3.bundle(function (err, buf) {
+        t.ok(bufferEqual(buf, fs.readFileSync('external/chain-output3.js')));
+    });
+});
