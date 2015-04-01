134a4567d78a7918fdc3580efc738e05b2facbab
Michael Wiencek <mwtuea@gmail.com>
2015-03-31 20:51:17 -0500

    don't bother caching for-loop length

--- src/util/first.js
+++ src/util/first.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function first(items, callback) {
-    for (var i = 0, len = items.length; i < len; i++) {
+    for (var i = 0; i < items.length; i++) {
         var result = callback(items[i]);
         if (result) {
             return result;
